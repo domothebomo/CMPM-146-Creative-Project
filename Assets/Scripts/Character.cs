fbd378223776217ae698fddb9380d490f0a23394
domothebomo <dfanaris@gmail.com>
2024-03-17 12:38:21 -0700

    Added pick up and drop functions to character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,6 +11,9 @@ public class Character : MonoBehaviour
     Vector3 move_destination = new Vector3(0.0f, 1.0f, 0.0f);
     NavMeshAgent agent;
 
+    Transform holdPos;
+    GameObject heldObject = null;
+
     GameObject waypoint;
 
     bool move_requested = false;
@@ -18,13 +22,24 @@ public class Character : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        holdPos = transform.GetChild(0);
 
+        /*waypoint = GameObject.Find("BasicObject");
+        Move(waypoint.transform.position);*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*if (Vector3.Distance(waypoint.transform.position, transform.position) <= 2 && heldObject == null)
+        {
+            PickUp(waypoint);
+        }*/
 
+        if (heldObject != null)
+        {
+            heldObject.transform.position = holdPos.position;
+        }
     }
 
     public void RequestMove()
@@ -55,6 +70,25 @@ public class Character : MonoBehaviour
 
     void Move(Vector3 position)
     {
+        Vector3 lookPos = position - transform.position;
+        lookPos.y = 0;
+        transform.rotation = Quaternion.LookRotation(lookPos);
         agent.destination = position;
+    }
+
+    public void PickUp(GameObject obj)
+    {
+        obj.GetComponent<Rigidbody>().useGravity = false;
+        obj.transform.position = holdPos.position;
+        obj.transform.parent = transform;
+        obj.transform.rotation = transform.rotation;
+        heldObject = obj;
+    }
+
+    public void Drop(GameObject obj)
+    {
+        obj.transform.parent = null;
+        obj.GetComponent<Rigidbody>().useGravity = true;
+        heldObject = null;
     }
 }
